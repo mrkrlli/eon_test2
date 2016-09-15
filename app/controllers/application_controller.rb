a10cf6b7f432a157611d3e1a420030839a3e231b
Mark Li <mark@philosophie.is>
2016-09-15 14:28:54 -0700

    Allow admin to invite users (#3)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def authenticate_inviter!
+    authenticate_admin_user!(:force => true)
+  end
 end
